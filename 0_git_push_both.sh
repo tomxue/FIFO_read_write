3637a01c111d8518f02f3b1ed78945833b787060
tomxue <tomxue@outlook.com>
2017-02-04 14:28:49 +0800

    update script

--- 0_git_push_both.sh
+++ 0_git_push_both.sh
@@ -1,9 +1,9 @@
-git config user.email tomxue0126@gmail.com
+git config --global user.email tomxue0126@gmail.com
 git config remote.origin.url git@github.com:tomxue/FIFO_read_write.git
 git push origin --mirror
 
 echo ""
-git config user.email tomxue@outlook.com
+git config --global user.email tomxue@outlook.com
 git config remote.origin.url ssh://tomxue@45.78.43.18:29418/0_Linux/UserSpace/IPC/FIFO_read_write.git
 git push origin --mirror
 
